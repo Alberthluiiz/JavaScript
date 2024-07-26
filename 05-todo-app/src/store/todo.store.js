4610b2fca8bb2fb8d84a760512192ca220d20294
Albertluiiz <luisguillen290301@gmail.com>
2024-07-26 02:04:44 -0500

    to-do

---
+++ 05-todo-app/src/store/todo.store.js
@@ -0,0 +1,26 @@
+import { Todo } from '../todos/models/todo.model';
+
+const Filters = {
+    All: 'all',
+    completed: 'Completed',
+    Pending: 'Penfinf'
+}
+
+const state = {
+    todos: [
+        new Todo('Piedra del alma'),
+        new Todo('Piedra del infinito'),
+        new Todo('Piedra del tiempo'),
+    ],
+    filter: Filters.All,
+}
+
+const initStore = () => {
+    console.log(state);    
+    console.log('InitStore 🥑');
+}
+
+
+export default{
+    initStore,
+}
